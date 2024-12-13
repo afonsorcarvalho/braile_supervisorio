dbfdf1dde72b53ffdf085efe11ff901db2d0a377
afonsorcarvalho <afonsorcarvalho@github.com>
2024-12-13 19:26:56 -0300

    update

--- __manifest__.py
+++ __manifest__.py
@@ -14,6 +14,7 @@
     'data': [
        
         'security/ir.model.access.csv',
+        'views/supervisorio_ciclo_views.xml',
         'reports/braile_supervisorio_ciclo_reports_template.xml',
         'reports/braile_supervisorio_ciclo_reports.xml',
        
